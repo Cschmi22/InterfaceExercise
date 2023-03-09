0eb8d3c49cc8ca111cce31744198fe7e56946601
Caleb Schmidt <cschmi14@gmail.com>
2023-03-09 11:26:59 -0500

    Done again hopefully

--- InterfaceExercise/Program.cs
+++ InterfaceExercise/Program.cs
@@ -8,22 +8,52 @@ namespace InterfaceExercise
     {
         static void Main(string[] args)
         {
-        var car = new Car();
+            var car = new Car()
+            {
+                HasTrunk = true,
+                SedanBody = true,
+                HasFourWheels = "Has 4 Wheels.",
+                Model = "Malibu",
+                Make = "Chevy",
+                SeatCount = 4,
 
-        var truck = new Truck();
+            };
 
-        var suv = new SUV();
+            var truck = new Truck()
+            {
+                HasBed = true,
+                CabSpace = "Extended",
+                HasFourWheels = "Has 4 Wheels.",
+                Model = "F-150",
+                Make = "Ford",
+                SeatCount = 4,
 
+            };
+
+            var suv = new SUV()
+            {
+                HasCargo = true,
+                IsSporty = true,
+                HasFourWheels = "Has 4 Wheels.",
+                Model = "RAV-4",
+                Make = "Toyota",
+                SeatCount = 5,
+            };
         var vehicles = new List<IVehicle>() { car, truck, suv};
 
         foreach (var vehicle in vehicles)
             {
+                Console.WriteLine($"The Make of this vehicle is {vehicle.Make}");
+                Console.WriteLine($"The Model of this vehicle is {vehicle.Model}");
+                Console.WriteLine($"This vehicle {vehicle.HasFourWheels}");
+                Console.WriteLine($"This vehicle has a seat count of {vehicle.SeatCount}");
+                Console.WriteLine();
                 vehicle.Drive();
                 vehicle.ChangeGears(true);
                 vehicle.Reverse();
-
+                Console.WriteLine();
+                Console.WriteLine();
             }
 
-        }
     }
 }
